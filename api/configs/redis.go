ac370b0f715e41e5f013f0240b4f5677bffbd4eb
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-08-27 20:28:46 +0700

    chore: get connection into redis

--- api/configs/redis.go
+++ api/configs/redis.go
@@ -1,1 +1,33 @@
 package configs
+
+import (
+	"api/utils"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var (
+	Redis *redis.Client
+)
+
+func GetRedisConnection() {
+	REDISDATABASE, err := strconv.Atoi(os.Getenv("REDISDATABASE"))
+	if err != nil {
+		REDISDATABASE = 0
+	}
+
+	Redis = redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDISHOST"),
+		DB: REDISDATABASE,
+	})
+
+	 _, err = Redis.Ping(utils.Ctx).Result()
+    if err != nil {
+        panic(err)
+    }
+
+	fmt.Println("Redis connection established successfully.")
+}

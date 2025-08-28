d610d16286ba603c74228933c598681d0280613c
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-08-28 11:45:38 +0700

    chore: change validator type to private

--- api/utils/validate/rules.go
+++ api/utils/validate/rules.go
@@ -2,18 +2,18 @@ package validate
 
 import "fmt"
 
-type Validator map[string]string
+type validator map[string]string
 
-func New() Validator {
-	return Validator{}
+func New() validator {
+	return validator{}
 }
 
-func (v Validator) Required(value interface{}, field string) {
+func (v validator) Required(value interface{}, field string) {
 	if value == "" || value == nil {
 		v[field] = fmt.Sprintf("The %s field is required.", field)
 	}
 }
 
-func (v Validator) Errors() bool {
+func (v validator) Errors() bool {
 	return len(v) > 0
 }

e12650824df803b7e54c2d8226f84d1680ea1c19
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-08-28 12:40:37 +0700

    chore: payload jsonTag tab

--- api/types/structs/payload.go
+++ api/types/structs/payload.go
@@ -1,7 +1,7 @@
 package structs
 
 type Payload struct {
-	Message string      `json:"message"`
-	Error   any `json:"error"`
-	Data    any `json:"data"`
+	Message string	`json:"message"`
+	Error   any 	`json:"error"`
+	Data    any 	`json:"data"`
 }

2c0b6d0312b7b2074677c238bdac067c02372f73
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-08-30 05:25:31 +0700

    chore: create password hashing utilities

---
+++ api/utils/password/hash.go
@@ -0,0 +1,15 @@
+package password
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Hash(password string) string {
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes)
+}
+
+func Verfify(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}

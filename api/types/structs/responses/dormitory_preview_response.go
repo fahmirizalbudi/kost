dc0b113187cc548a0f60cc2b88680bc3d7804d39
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-08-30 19:52:12 +0700

    fix: change typo in struct

--- api/types/structs/responses/dormitory_preview_response.go
+++ api/types/structs/responses/dormitory_preview_response.go
@@ -4,7 +4,7 @@ import "time"
 
 type DormitoryPreviewResponse struct {
 	ID          int       `json:"id"`
-	DormitoryID int       `json:"formitory_id"`
+	DormitoryID int       `json:"dormitory_id"`
 	Url         string    `json:"url"`
 	CreatedAt   time.Time `json:"created_at"`
 }

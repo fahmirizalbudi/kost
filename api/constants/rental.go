34c61af2c83d257afb322b27107551bb99907c43
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-09-01 13:54:13 +0700

    chore: create rental status constants

---
+++ api/constants/rental.go
@@ -0,0 +1,6 @@
+package constants
+
+const RENTAL_STATUS_PENDING = "pending"
+const RENTAL_STATUS_ACTIVE = "active"
+const RENTAL_STATUS_FINISHED = "finished"
+const RENTAL_STATUS_CANCELLED = "cancelled"

8a30ba1dd26afae8d43e546bd37f8de16a99ecb1
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-09-03 18:45:00 +0700

    chore: add dormitory_price, month_paid column and remove status in transactions

--- api/database/migrations/006_create_table_transactions.sql
+++ api/database/migrations/006_create_table_transactions.sql
@@ -2,14 +2,18 @@
 -- +migrate StatementBegin
 
 CREATE TYPE transaction_method AS ENUM ('cash', 'transfer', 'ewallet');
-CREATE TYPE transaction_status AS ENUM ('pending', 'failed', 'paid');
+CREATE TYPE transaction_purpose AS ENUM ('new', 'renewal');
+
+-- TODO: add dormitory_price column, add purpose column ('new', 'renewal')
 
 CREATE TABLE transactions (
     id SERIAL PRIMARY KEY,
     rental_id INT NOT NULL,
+    dormitory_price INT NOT NULL,
+    month_paid INT NOT NULL,
     amount INT NOT NULL,
     method transaction_method NOT NULL,
-    status transaction_status DEFAULT 'pending',
+    purpose transaction_purpose DEFAULT 'new',
     proof TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -21,6 +25,6 @@ CREATE TABLE transactions (
 
 DROP TABLE IF EXISTS transactions;
 DROP TYPE IF EXISTS transaction_method;
-DROP TYPE IF EXISTS transaction_status;
+DROP TYPE IF EXISTS transaction_purpose;
 
 -- +migrate StatementEnd

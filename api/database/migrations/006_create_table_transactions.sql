05287839ee96b36093deca7de01cc75147d7a350
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-09-05 09:29:20 +0700

    chore: change transaction id to varchar and add status column to transactions table

--- api/database/migrations/006_create_table_transactions.sql
+++ api/database/migrations/006_create_table_transactions.sql
@@ -3,17 +3,19 @@
 
 CREATE TYPE transaction_method AS ENUM ('cash', 'transfer', 'ewallet');
 CREATE TYPE transaction_purpose AS ENUM ('new', 'renewal');
+CREATE TYPE transaction_status AS ENUM ('pending', 'success');
 
 -- TODO: add dormitory_price column, add purpose column ('new', 'renewal')
 
 CREATE TABLE transactions (
-    id SERIAL PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     rental_id INT NOT NULL,
     dormitory_price INT NOT NULL,
     month_paid INT NOT NULL,
     amount INT NOT NULL,
     method transaction_method NOT NULL,
     purpose transaction_purpose DEFAULT 'new',
+    status transaction_status DEFAULT 'pending',
     proof TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -26,5 +28,6 @@ CREATE TABLE transactions (
 DROP TABLE IF EXISTS transactions;
 DROP TYPE IF EXISTS transaction_method;
 DROP TYPE IF EXISTS transaction_purpose;
+DROP TYPE IF EXISTS transaction_status;
 
 -- +migrate StatementEnd

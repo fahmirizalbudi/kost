6b58d9a29581b2ef77aa07afe4abc60a2aa5bf0d
fahmirizalbudi <fahmirizal.dev@gmail.com>
2025-09-05 15:23:32 +0700

    chore: init font in layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,6 +2,26 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./styles/globals.scss";
 
+const sfProText = localFont({
+  src: [
+    { path: '/fonts/SFProText-Regular.ttf', weight: '400', style: 'normal' },
+    { path: '/fonts/SFProText-RegularItalic.ttf', weight: '400', style: 'italic' },
+
+    { path: '/fonts/SFProText-Medium.ttf', weight: '500', style: 'normal' },
+    { path: '/fonts/SFProText-MediumItalic.ttf', weight: '500', style: 'italic' },
+
+    { path: '/fonts/SFProText-Semibold.ttf', weight: '600', style: 'normal' },
+    { path: '/fonts/SFProText-SemiboldItalic.ttf', weight: '600', style: 'italic' },
+
+    { path: '/fonts/SFProText-Bold.ttf', weight: '700', style: 'normal' },
+    { path: '/fonts/SFProText-BoldItalic.ttf', weight: '700', style: 'italic' },
+
+    { path: '/fonts/SFProText-Heavy.ttf', weight: '900', style: 'normal' },
+    { path: '/fonts/SFProText-HeavyItalic.ttf', weight: '900', style: 'italic' },
+  ],
+  variable: '--font-sfpro',
+});
+
 export const metadata: Metadata = {
   title: "Kostopia - Rent a Dorm",
   description: "App that used for rent an dorm",
@@ -14,7 +34,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={``}>
+      <body className={`${sfProText.variable}`}>
         {children}
       </body>
     </html>
